3cea721a2cd5681326e1ecb27555d79db7b30bca
emoshu-kenshin-kawai <kenshin.kawai@emoshu_co.jp>
2023-02-16 14:05:49 +0900

    member joins employmentstatus and status

--- interfaces/database/member_repository.go
+++ interfaces/database/member_repository.go
@@ -9,7 +9,7 @@ type MemberRepository struct {
 }
 
 func (repo *MemberRepository) FindById(id string) (member domain.Member, err error) {
-	if err = repo.First(&member, id).Error; err != nil {
+	if err = repo.Joins("EmploymentStatus").Joins("Status").First(&member, id).Error; err != nil {
 		return member, err
 	}
 	return member, err
