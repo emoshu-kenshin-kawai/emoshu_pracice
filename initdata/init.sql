d5f8c1294abaaaceffd8998cd6b4721c1f31eb62
emoshu-kenshin-kawai <kenshin.kawai@emoshu_co.jp>
2023-02-21 16:59:18 +0900

    create initial sql data file

---
+++ initdata/init.sql
@@ -0,0 +1,22 @@
+INSERT INTO emoshu_practice_dev.departments (department) VALUES
+('technology'),
+('public-relations'),
+('business'),
+('sales');
+
+INSERT INTO emoshu_practice_dev.statuses (status) VALUES
+('at-work'),
+('break'),
+('left');
+
+INSERT INTO emoshu_practice_dev.roles (role) VALUES
+('CEO'),
+('CTO'),
+('junior-engineer'),
+('senior-engineer');
+
+INSERT INTO emoshu_practice_dev.employment_statuses (employment_status) VALUES
+('part-time'),
+('full-time'),
+('internship'),
+('contract-employee');
